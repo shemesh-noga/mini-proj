ff92cc28c65fa89ca95ad6ecd36e0cbd79dec755
Moriya <moriyakatsman@hilma.tech>
2024-11-26 12:49:51 +0200

    begining of post teacher

--- mini-project/server/routes/teacher.js
+++ mini-project/server/routes/teacher.js
@@ -3,9 +3,28 @@ var router = express.Router();
 const mysql = require("mysql");
 const path = require("path");
 
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
+});
+
+router.post("/", async (req, res, next) => {
+  // expected body: {name: , password: , email: }
+  try {
+    if (!req.body.name || !req.body.password)
+      throw new Error("Missing name and/or password in the request's body.");
+
+    checkTeacher(req.body.name, req.body.password);
+    
+
+    if (checkThisAdmin === false)
+      throw new Error("Access denied, no admin found");
+    res.send(`name: ${req.body.name}, password: ${req.body.password}`);
+  } catch (err) {
+    console.error(err);
+    res.status(404).send(`Error: Couldn't add school, ${err.message}`);
+  }
 });
 
 module.exports = router;
